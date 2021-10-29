92d455f7c3229dc74fc62a9d313ae6c2b89e1d24
Valentín Osorio <dosoriom@unal.edu.co>
2021-10-28 22:15:54 -0500

    2

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,19 @@
+package taller3.televisores;
+
+public class TV {
+    private Marca marca;
+    private int canal;
+    private int precio;
+    private boolean estado;
+    private int volumen;
+    private Control control;
+    private static int numtv;
+
+    public TV (Marca marca, boolean estado){
+        this.marca = marca;
+        this.estado = estado;
+    }
+
+    
+    
+}
