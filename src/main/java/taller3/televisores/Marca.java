dd07b69d27cf553e486e4150274383d3c4c1af72
Valentín Osorio <dosoriom@unal.edu.co>
2021-10-28 22:15:07 -0500

    Aun sin terminar

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,19 @@
+package taller3.televisores;
+
+public class Marca {
+
+    String nombre;
+
+    public Marca (String nombre) {
+        this.nombre = nombre;
+    }
+
+    String getNombre() {
+        return nombre;
+    }
+
+    void setNombre(String nombre) {
+        this.nombre = nombre; 
+    }
+
+}
